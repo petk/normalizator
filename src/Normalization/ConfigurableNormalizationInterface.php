23f982dbbb478ea6df4bd2c4fe660240bd8f2a4b
Peter Kokot <peterkokot@gmail.com>
2023-06-19 19:23:22 +0200

    Add missing docs for configurable normalization interface

--- src/Normalization/ConfigurableNormalizationInterface.php
+++ src/Normalization/ConfigurableNormalizationInterface.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Normalizator\Normalization;
 
 /**
- * Interface for normalizations where.
+ * Interface for normalizations where additional configuration is supported.
  */
 interface ConfigurableNormalizationInterface extends NormalizationInterface
 {
