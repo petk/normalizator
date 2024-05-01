c43622c4e1857ede83bbe36153bb4c8929b7d6a8
Peter Kokot <peterkokot@gmail.com>
2024-05-01 21:50:01 +0200

    Start with 0.0.6-dev version

--- src/Normalizator.php
+++ src/Normalizator.php
@@ -19,7 +19,7 @@ use function array_merge;
  */
 class Normalizator implements NormalizatorInterface
 {
-    public const VERSION = '0.0.5';
+    public const VERSION = '0.0.6-dev';
 
     public function __construct(
         private Configuration $configuration,
